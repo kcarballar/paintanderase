557a491230d42d752c31c6f4b0cbc117e8f2c0d2
kcarballar <kcarballar@gmail.com>
2016-10-04 13:49:18 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas (600,400);
+  background (100,200,0);
+}
+
+function draw() {
+  noStroke()
+  fill (100,100,100,100);
+  ellipse (mouseX, mouseY,25,25);
+  
+}
+
+function mousePressed() {
+background (100,200,0);
+}
